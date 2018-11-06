21612f9dc2bf16bda031dfaf8e4053c876e67850
nicktming <2279563278@qq.com>
2018-11-06 23:35:23 +0800

    change color

--- src/main/java/com/example/myspinjenkindemo/controllers/HomeController.java
+++ src/main/java/com/example/myspinjenkindemo/controllers/HomeController.java
@@ -11,7 +11,7 @@ public class HomeController {
 
     @GetMapping
     public String home() {
-        return "<body style=\"background-color:black\">\n" +
+        return "<body style=\"background-color:green\">\n" +
                 "    <h2>Hello, spinnaker!</h2>\n" +
                 "</body>";
     }
